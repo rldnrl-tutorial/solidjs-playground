20b0cef5fb54b50923234c81319d7f48f9b8ecce
rldnrl <devpark1002@gmail.com>
2022-07-11 22:26:33 +0900

    feat: onSave Props 추가

--- src/libs/ui/Card/Card.tsx
+++ src/libs/ui/Card/Card.tsx
@@ -6,9 +6,16 @@ type CardProps = {
   description?: string;
   starCount?: number;
   to?: string;
+  onSave?: () => void;
 };
 
-const Card: Component<CardProps> = ({ title, description, starCount, to }) => {
+const Card: Component<CardProps> = ({
+  title,
+  description,
+  starCount,
+  to,
+  onSave,
+}) => {
   return (
     <div class="my-4 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
       <div class="p-5">
@@ -21,7 +28,10 @@ const Card: Component<CardProps> = ({ title, description, starCount, to }) => {
         <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
           {description}
         </p>
-        <button class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        <button
+          class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          onClick={onSave}
+        >
           Save
         </button>
       </div>
