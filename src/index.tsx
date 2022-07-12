6bff3844130eac5f964b34bb8af6b5621c541f60
rldnrl <devpark1002@gmail.com>
2022-07-12 20:13:08 +0900

    feat: wrapping repo provider

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,14 @@ import { Router } from "solid-app-router";
 
 import "./index.css";
 import App from "./App";
+import RepoProvider from "./libs/providers/RepoProvider";
 
 render(
   () => (
     <Router>
-      <App />
+      <RepoProvider>
+        <App />
+      </RepoProvider>
     </Router>
   ),
   document.getElementById("root") as HTMLElement
