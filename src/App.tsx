dd5c4357dcf7d13c2aa9b0657feb61563c1bf17e
rldnrl <devpark1002@gmail.com>
2022-07-17 14:58:51 +0900

    style: change width

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import { Route, Routes } from "solid-app-router";
 import type { Component } from "solid-js";
+import { Route, Routes } from "solid-app-router";
+
 import Nav from "./libs/ui/Nav";
 import Home from "./pages/Home";
 import SavedRepos from "./pages/SavedRepos";
 
 const App: Component = () => {
   return (
-    <div class="px-4 pt-4">
+    <div class="w-96 px-4 pt-4">
       <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
