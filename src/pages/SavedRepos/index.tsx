5ee4a9d40aff2391a7d83c4061a03302d3977c0a
rldnrl <devpark1002@gmail.com>
2022-07-17 16:18:33 +0900

    feat: store 적용

--- src/pages/SavedRepos/index.tsx
+++ src/pages/SavedRepos/index.tsx
@@ -1,15 +1,13 @@
 import { Component, createSignal, For } from "solid-js";
-import { useRepoContext } from "../../libs/providers/RepoProvider";
 
+import { savedRepos } from "../../libs/store/repo";
 import RepoCard from "../../libs/ui/RepoCard";
 
 const SavedRepos: Component = () => {
-  const { savedRepos } = useRepoContext();
-
   return (
     <div>
       <h2>Saved Repos</h2>
-      <For each={savedRepos}>{(repo) => <RepoCard repo={repo} />}</For>
+      <For each={savedRepos()}>{(repo) => <RepoCard repo={repo} />}</For>
     </div>
   );
 };
